3a3102697d5bce6b096b1e47969cbeb1248e7b18
qkqajrrhtj <qkqajrrhtj@naver.com>
2024-04-25 20:44:16 +0900

    로그인 및 토큰 쿠키저장

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const User = require('./models/User');
 const Config = require('./config/key');
+const cookieParser = require('cookie-parser');
 
 //bodyParser 옵션주기 =>이런 데이터들을 처리해서 파싱할 수 있도록
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
+//cookieParser
+app.use(cookieParser());
 
 //몽고 db가져오기
 const mongoose = require('mongoose');
@@ -33,7 +36,7 @@ app.post('/register', async (req, res) => {
   //body-parser를 이용해서 body에 데이터를 실어 보낼수있다.
   const user = new User(req.body);
   //save는 mongo매서드
-  const result = await user
+  await user
     .save()
     .then(() => {
       //성공하면 성공했다는 status200과 json 객체 전달
@@ -45,6 +48,44 @@ app.post('/register', async (req, res) => {
       //실패하면
       res.json({ success: false, err });
     });
+});
+
+//login요청
+app.post('/api/users/login', async (req, res) => {
+  try {
+    //먼저 요청한 email이 db에 있는지 확인
+    let userInfo = await User.findOne({ email: req.body.email });
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        msg: '제공된 이메일에 해당하는 유저가 없습니다',
+      });
+    }
+    // db에 있다면 맞는 비밀번호인지 확인. comparePassword매서드는 User.js에서 생성해야함.
+    //일치하는게 있다면 isMatch가 true값
+    const isMatch = await userInfo.comparePassword(req.body.password, userInfo);
+    //isMatch가 false라면 => 비밀번호가 같지 않다는 것(User.js파일에서 그렇게 리턴함)
+    if (!isMatch) {
+      return res.json({
+        loginSuccess: false,
+        msg: '비밀번호가 틀렸습니다',
+      });
+    }
+    //비밀번호까지 맞다면 토큰 생성. generateToken()매서드도 User.js에서 생성해야함
+    const jwtData = await userInfo.generateToken(userInfo);
+    if (!jwtData.token) {
+      //제대로 저장이안됐다면 400상태와 에러메세지 같이 전달 =>response로
+      return res.status(400).send(jwtData.err);
+    }
+    //user에 있는 토큰을 저장해야한다. =>어디에? 쿠키, 로컬스토리지, 세션.... 여기선 쿠키에
+    //'x_auth'라는 이름으로 user에 있는 토큰 저장
+    res
+      .cookie('x_auth', jwtData.token)
+      .status(200)
+      .json({ loginSuccess: true, userId: jwtData._id });
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 //스키마는 db틀을 정한다고 생각하면 됨
 const userSchema = mongoose.Schema({
@@ -63,6 +64,33 @@ userSchema.pre('save', function (next) {
     next();
   }
 });
+
+//비밀번호 비교하는 매서드 만들기 (bcrypt를 이용한 암호화된 것과 비교)
+userSchema.methods.comparePassword = async (plainPassword, userInfo) => {
+  //plainPassword를 암호화해서 이미 암호화된 db비밀번호와 대조
+  let isMatch = await bcrypt.compare(plainPassword, userInfo.password);
+  // isMatch가 있다면 null, true값의 isMatch리턴
+  if (isMatch) {
+    return isMatch;
+  }
+  // isMatch가 없다면 false리턴
+  return (isMatch = false);
+};
+
+//토큰 생성하는 매서드 만들기
+userSchema.methods.generateToken = async (userInfo) => {
+  //jsonwebtoken을 이용해서 token생성하기
+  //uesr의 _id와 secretToken이라는 문자열을 더해서 토큰 생성
+  let token = jwt.sign(userInfo._id.toHexString(), 'secretToken');
+  // 만든 토큰을 이 유저 token정보에 저장
+  userInfo.token = token;
+  //더이상 save가 안에서 콜백함수를 지원하지 않기 때문에 then과 catch를 이용해야함.
+  //지금은 res인자를 사용하는 것이 아니고 return해줘야하므로 이렇게 작성
+  const result = await userInfo.save();
+  if (result) {
+    return result;
+  }
+};
 
 //모델로 감싸기 => 이름이 User이고 틀이 userSchema인 모델 생성
 const User = mongoose.model('User', userSchema);
