d0aabfe03cb4d8504ac31e47d092439b21257848
qkqajrrhtj <qkqajrrhtj@naver.com>
2024-05-05 22:15:54 +0900

    로그아웃 프론트

--- client/src/components/views/landingPage/LandingPage.js
+++ client/src/components/views/landingPage/LandingPage.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     axios
       .get('/api/hello')
@@ -12,7 +14,32 @@ const LandingPage = () => {
         console.log(err);
       });
   }, []);
-  return <div>랜딩 페이지</div>;
+  //로그아웃 요청
+  const logout = () => {
+    axios
+      .get('/api/users/logout')
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.success === true) {
+          navigate('/login');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      랜딩 페이지
+      <button
+        onClick={() => {
+          logout();
+        }}
+      >
+        log out
+      </button>
+    </div>
+  );
 };
 
 export default LandingPage;
